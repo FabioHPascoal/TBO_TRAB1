5bd67426d12121975ca41b3a2cd222334327badb
nathangarciaf <nathangarfreitas@gmail.com>
2025-03-22 00:31:04 -0300

    comentários da bst

--- bst.h
+++ bst.h
@@ -8,18 +8,54 @@ typedef int (*CmpFn)(void *, void *);
 typedef struct Node Node;
 typedef struct BST BST;
 
+/*
+ * Crie uma árvore binária vazia com a funçãp de comparação para busca e inserção
+ */
 BST *bst_construct(CmpFn cmp_fn);
+
+/*
+ * Destroi a árvore binária
+ */
 void bst_destroy(BST *bst);
 
+/*
+ * Insere uma chave na arvore binária
+ */
 void bst_add(BST *bst, void *data);
+
+/*
+ * Remove uma chave na arvore binária
+ */
 void bst_remove(BST *bst, void *key);
 
+/*
+ * Obtém o item de menor chave da árvore binária
+ */
 void *bst_min(BST *bst);
+
+/*
+ * Obtém o item de maior chave da árvore binária
+ */
 void *bst_max(BST *bst);
+
+/*
+ * Remove o item de menor chave da árvore binária
+ */
 void *bst_pop_min(BST *bst);
+
+/*
+ * Remove o item de maior chave da árvore binária
+ */
 void *bst_pop_max(BST *bst);
 
+/*
+ * Retorna a quantidade de nós na árvore
+ */
 int bst_node_count(BST *bst);
+
+/*
+ * Retorna se a árvore ta vázia
+ */
 bool bst_is_empty(BST *bst);
 
 #endif
